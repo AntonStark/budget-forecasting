0ac3f42f2cd7a08f22dde004a017a0dfdca5f2a2
Anton Iakovlev <lantar23@yandex.ru>
2023-12-09 19:08:13 +0300

    set title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import "@/styles/common.css"
+import Head from "next/head";
+
+import "@/styles/common.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Бюджет</title>
+                <link rel="shortcut icon" href="/icon.svg" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
